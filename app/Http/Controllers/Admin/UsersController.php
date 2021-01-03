2f7748c9c403b54f536abfc0c49e2ba95bd4380d
ola-cpu <Hackerdetoday@gmail.com>
2021-01-03 16:28:54 +0100

    ajout des modification

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -65,7 +66,14 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        //
+
+        $roles = Role::all();
+
+        return view('admin.users.edit', [
+            'user' => $user,
+            'roles' => $roles
+
+        ]);
     }
 
     /**
@@ -77,7 +85,10 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        
+         $user->roles()->sync($request->roles);
+
+         return redirect()->route('admin.users.index');
     }
 
     /**
@@ -88,6 +99,11 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->roles()->detach();
+        $user->delete();
+
+
+        return redirect()->route('admin.users.index');
+
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,16 +2,64 @@
 
 @section('content')
 <div class="container">
+
     <div class="row justify-content-center">
+
         <div class="col-md-8">
+
             <div class="card">
+
                 <div class="card-header">Liste des utilisateur</div>
 
                 <div class="card-body">
-                @foreach ($users as $user)
+                    <table class="table">
 
-                    {{ $user->name}} - {{ $user->email}}
-                @endforeach
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Roles</th>
+                            <th scope="col">Action</th>
+                        </tr>
+
+                    </thead>
+
+                    <tbody>
+
+                         @foreach ($users as $user)
+
+                             <tr>
+                                
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ implode(', ' , $user->roles()->get()->pluck('name')->toArray()) }}</td>
+                                
+                                <td>
+                                 <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary">Editer</button></a>   
+                                 
+                                 <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <a href=""><button class="btn btn-warnig">Suprimer</button></a>   
+                                     
+                                 </form>
+
+                                 
+
+                                </td>
+
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                        
+
+                    </table>
+
+               
 
                 </div>
             </div>
